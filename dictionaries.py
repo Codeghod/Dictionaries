e3b6f328373469c840ebd9d8c63a51421b591f02
Alfred Turkson <111619866+Codeghod@users.noreply.github.com>
2022-09-18 15:19:57 -0700

    Add files via upload

---
+++ dictionaries.py
@@ -0,0 +1,6 @@
+# A tuple is a container which stores key value pairs
+# A tuple key is the one which comes before the (:) colon sign and the values come after the colon.
+
+
+dc = {"name":"Alfred","Age":24,"class":"level 300"}
+print(dc)
