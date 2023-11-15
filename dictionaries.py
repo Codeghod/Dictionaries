b8d8a7beb46db1823dc2a528b641c1dfbdadbf84
Alfred Kojo Turkson <111619866+Codeghod@users.noreply.github.com>
2023-11-15 08:59:37 +0000

    Update dictionaries.py

--- dictionaries.py
+++ dictionaries.py
@@ -1,5 +1,5 @@
-# A tuple is a container which stores key value pairs
-# A tuple key is the one which comes before the (:) colon sign and the values come after the colon.
+# A dictionary is a container which stores key value pairs
+# A dictionary key is the one which comes before the (:) colon sign and the values come after the colon.
 
 
 dc = {"name":"Alfred","Age":24,"class":"level 300"}
